58b993779fb9cc6d867be68a5bef669e84dbeb1e
Praizl <orirepraiz98@gmail.com>
2024-09-28 21:29:19 +0100

    side with zero

--- geometry/cube.go
+++ geometry/cube.go
@@ -1,5 +1,12 @@
 package geometry
 
-func Cube(side int) int {
-	return side * side * side
+import "errors"
+
+func Cube(side int) (int, error) {
+	if side != 0 {
+		return side * side * side, nil
+	} else {
+		return 0, errors.New("cube side dimension invalid")
+
+	}
 }
